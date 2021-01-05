c3f94c87d32c2d5396d5fc6e784de81a1d95f90e
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2021-01-06 00:22:35 +0100

    Add rand input (albero_binario_Zanotti.cpp)

--- exe_1012/albero_binario_Zanotti.cpp
+++ exe_1012/albero_binario_Zanotti.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -9,26 +11,28 @@ struct nodo{
 
 nodo *NuovoNodo(int dato);
 
-void RiempiAlbero(nodo *&root);
+void RiempiAlbero(nodo *&root, int MAX);
 
 void StampaFoglie(nodo *root);
 
 int main(){
+    const int MAX = 100;
     nodo *root = NULL;
 
     cout << endl;
     cout << "     *****************************************" << endl;
     cout << "      Developed by Paolo Zanotti @ UniBg 2021 " << endl;
     cout << "     *****************************************" << endl;
-    cout << endl << endl;
+    cout << endl;
 
-    RiempiAlbero(root);
+    RiempiAlbero(root, MAX);
 
     StampaFoglie(root);
 
     return 0;
 }
 
+// Generate the node
 nodo *NuovoNodo(int dato){
     nodo *temp = new nodo;
     temp->dato = dato;
@@ -37,31 +41,39 @@ nodo *NuovoNodo(int dato){
     return temp;
 }
 
-//TODO: recursive
-void RiempiAlbero(nodo *&root){
-    root = NuovoNodo(10);
-    root->left = NuovoNodo(9);
-    root->right = NuovoNodo(8);
-    root->left->left = NuovoNodo(5);
-    root->left->right = NuovoNodo(4);
-    root->right->left = NuovoNodo(6);
-    root->right->right = NuovoNodo(2);
+// Add rand items
+void RiempiAlbero(nodo *&root, int MAX){
+    srand((unsigned int)time(0));
+    
+    root = NuovoNodo(rand() % MAX + 1);
+    root->left = NuovoNodo(rand() % MAX + 1);
+    root->right = NuovoNodo(rand() % MAX + 1);
+    root->left->left = NuovoNodo(rand() % MAX + 1);
+    root->left->right = NuovoNodo(rand() % MAX + 1);
+    root->right->left = NuovoNodo(rand() % MAX + 1);
+    root->right->right = NuovoNodo(rand() % MAX + 1);
 }
 
+// Print leaves (recursive)
 void StampaFoglie(nodo *root){
+
+    // Check if the node is null
     if(!root)
         return;
     
+    // Check if the node is a leaf (left->NULL && right->NULL)
     if(!root->left && !root->right){
-        cout << "Foglia: " << root->dato << endl;
+        cout << "\t* Foglia dell'albero binario: " << root->dato << endl << endl;
         
         return;
     }
 
+    // Check if the left child of node exists
     if(root->left){
         StampaFoglie(root->left);
     }
 
+    // Check if the right child of node exists
     if(root->right){
         StampaFoglie(root->right);
     }
